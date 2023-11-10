b7eacc7cea1b100d0009430447a3378edd62f69f
steve12d <sdictor@renew-ap.com>
2023-11-10 14:32:28 +0000

    Updated long description and Github workflow

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='kubestatus',
-    version='0.1.1',
+    version='0.1.2',
     author='Stephen Dictor',
     author_email='sdictor@renew-ap.com',
     long_description=long_description,
